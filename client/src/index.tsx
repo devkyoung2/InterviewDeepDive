0650a43e12808da48f8d27e5c6ac1759cc6fd46c
devkyoung2 <devkyoung2@gmail.com>
2024-08-24 18:05:13 +0900

    [chore] : 프론트 환경 세팅 및 빌드시스템 구축
    - webpack 설정, webpack dev server 설정, 바벨 등 패키지 설치

---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<div>하이 ㅋㅋ</div>);
+}
