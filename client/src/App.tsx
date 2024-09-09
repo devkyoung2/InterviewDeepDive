10d9d4f682ccaf9a796f76146143dd13b24a8ab2
devkyoung2 <devkyoung2@gmail.com>
2024-09-10 00:18:12 +0900

    [feat] : 반응형으로 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import JavaScript from '../assets/JavaScript.json';
 
 type TQuestion = {
@@ -21,23 +21,49 @@ const App = () => {
     if (btnText === '시작하기') setBtnText('다른문제');
   };
 
+  const disableScroll = () => {
+    document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
+    document.body.style.height = '100%';
+
+    return () => {
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.width = '';
+      document.body.style.height = '';
+    };
+  };
+
+  useEffect(() => disableScroll(), []);
+
   return (
-    <div className='h-screen w-screen bg-emerald-700 flex flex-col justify-center items-center'>
-      <h1 className='h-1/6 font-PartialSansKR text-6xl text-white drop-shadow-md'>
+    <div className='h-screen w-full bg-emerald-700 text-center p-10 md:py-20 flex flex-col'>
+      <h1 className='text-3xl sm:text-4xl md:text-5xl text-white drop-shadow-md font-PartialSansKR pb-11'>
         면접질문뽑기
       </h1>
-      <section className='bg-white  text-center h-3/5 w-3/5 rounded-3xl shadow-md fl p-8 font-GoormSansBold whitespace-pre-wrap'>
-        <p className='h-4/5 p-7 flex justify-center items-center text-3xl '>
-          {question}
-        </p>
-        <button
-          type='button'
-          className='w-48 rounded-3xl shadow-md bg-emerald-400 p-5 text-xl text-white'
-          onClick={selectQuestion}
-        >
-          {btnText}
-        </button>
-      </section>
+      <div className='grid grid-cols-1 lg:grid-cols-[4fr_1fr] gap-2 lg:gap-8 lg:h-full md:px-20 2xl:px-40 font-GoormSansBold '>
+        <div className='bg-white rounded-xl px-4 py-9 relative flex flex-col justify-between items-center h-[250px] lg:h-auto'>
+          <div className='grow flex items-center justify-center text-2xl sm:text-3xl xl:text-4xl'>
+            {question}
+          </div>
+          <button
+            type='button'
+            className='w-56 sm:w-80 xl:w-96 rounded-full shadow-md bg-emerald-300 h-12 text-base sm:text-xl text-white'
+            onClick={selectQuestion}
+          >
+            {btnText}
+          </button>
+        </div>
+        <div className='flex flex-col lg:min-w-[240px]'>
+          <div className='bg-emerald-100 rounded-xl grow p-4 h-[120px]'>
+            <p>🖋️기록🖋️</p>
+          </div>
+          <div className='bg-emerald-100 rounded-xl h-[120px] lg:h-36  mt-2 lg:mt-7 p-4 lg:min-w-[240px]'>
+            <p>🖋️타이머🖋️</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
