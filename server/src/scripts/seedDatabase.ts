99995ba6db88908ddc826363875484758736f47a
devkyoung2 <devkyoung2@gmail.com>
2024-09-16 02:15:32 +0900

    [feat] : 질문 추가 스크립트 구현 및 JS 질문 추가

---
+++ server/src/scripts/seedDatabase.ts
@@ -0,0 +1,158 @@
+import CONFIG from '../config';
+import Question from '../models/Question';
+import mongoose from 'mongoose';
+
+const questionsData = [
+  { question: 'ES6에 추가된 문법에 대해 설명해주세요', category: 'JavaScript' },
+  {
+    question: "'var','const','let'의 차이점에 대해 설명해주세요",
+    category: 'JavaScript',
+  },
+  {
+    question: "'null', 'undefined' 의 차이점에 대해 설명해주세요",
+    category: 'JavaScript',
+  },
+  { question: "'NaN'이 무엇인가요?", category: 'JavaScript' },
+  {
+    question: 'JS에서 세미콜론이 옵션인 이유에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  { question: "'Symbol'에 대해 설명해주세요", category: 'JavaScript' },
+  {
+    question: "'==' 과 '===' 의 차이에 대해 설명해주세요",
+    category: 'JavaScript',
+  },
+  {
+    question: "'for … of'와 'for … in' 문의 차이에 대해 설명해주세요",
+    category: 'JavaScript',
+  },
+  { question: "'reduce'사용법에 대해 알고있나요?", category: 'JavaScript' },
+  {
+    question: "'splice'와 'slice' 차이점을 설명해주세요",
+    category: 'JavaScript',
+  },
+  { question: '옵셔널 체이닝에 대해 설명해주세요', category: 'JavaScript' },
+  { question: "NULL 병합 연산자('??')는 무엇인가요?", category: 'JavaScript' },
+  { question: 'JS의 단축평가에 대해 설명해주세요', category: 'JavaScript' },
+  { question: 'Promise에 대해 설명해주세요', category: 'JavaScript' },
+  { question: '클로저(Closure)란 무엇인가요?', category: 'JavaScript' },
+  {
+    question: '구조 분해 할당(Destructuring assignment)이 무엇인가요?',
+    category: 'JavaScript',
+  },
+  { question: '전개 구문(Spread syntax)이란?', category: 'JavaScript' },
+  { question: '나머지 매개변수(Rest parameters)란?', category: 'JavaScript' },
+  { question: "'use strict'가 무엇인가요?", category: 'JavaScript' },
+  {
+    question: "'apply', 'call', 'bind'에 대해 설명해주세요",
+    category: 'JavaScript',
+  },
+  {
+    question:
+      '함수 선언문, 함수 표현식, 화살표 함수의 차이에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '익명 함수(anonymous function)에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question:
+      '즉시 실행 함수 표현(IIFE, Immediately Invoked Function Expression)이 무엇인가요?',
+    category: 'JavaScript',
+  },
+  {
+    question: '콜백함수와 콜백 지옥에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '순수함수와 비순수함수의 차이점을 설명해주세요',
+    category: 'JavaScript',
+  },
+  { question: '고차함수에 대해 설명해주세요', category: 'JavaScript' },
+  { question: '일급객체가 무엇인가요?', category: 'JavaScript' },
+  { question: "'this'에 대해 아는대로 설명해주세요", category: 'JavaScript' },
+  { question: '클래스에 대해 알고있나요?', category: 'JavaScript' },
+  {
+    question: '자바스크립트에서 불변성은 무엇을 의미할까요?',
+    category: 'JavaScript',
+  },
+  {
+    question: '자바스크립트 자료형 중에 객체와 다른 타입 간의 차이가 있을까요?',
+    category: 'JavaScript',
+  },
+  { question: '프로토타입이란 무엇인가요?', category: 'JavaScript' },
+  {
+    question: '(Weak)Map과 (Weak)Set에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  { question: "'Date' 객체를 사용해본적이 있나요?", category: 'JavaScript' },
+  {
+    question: '얕은 복사와 깊은 복사의 차이점에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '문(statement)과 표현식(expression)의 차이점에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: 'JavaScript Module System과 Dynamic import?',
+    category: 'JavaScript',
+  },
+  {
+    question: 'node.js와 그냥 js의 차이점에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  { question: '유사 배열 객체에 대해 설명해주세요', category: 'JavaScript' },
+  { question: 'generator에 대해 설명해주세요', category: 'JavaScript' },
+  { question: 'polyfill이란 무엇인가요?', category: 'JavaScript' },
+  {
+    question: '자바스크립트 엔진의 동작 방법에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '이벤트 루프와 Task queue에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '호이스팅(hoisting)에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  { question: '실행 컨텍스트에 대해 설명해주세요', category: 'JavaScript' },
+  { question: '스코프에 대해 아는대로 설명해주세요', category: 'JavaScript' },
+  {
+    question: 'JS의 가비지 컬렉션(GC)에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question:
+      '배열에 값을 추가/삭제했을 때 메모리가 어떻게 변화하는지 과정을 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question:
+      '자바스크립트는 싱글 스레드 기반인데 어떻게 비동기 처리를 할 수 있나요?',
+    category: 'JavaScript',
+  },
+  {
+    question:
+      '블록 레벨 스코프와 함수 레벨 스코프의 차이점에 대해 설명해주세요.',
+    category: 'JavaScript',
+  },
+];
+
+async function insertMockData() {
+  try {
+    await mongoose.connect(CONFIG.DB.HOST);
+    console.log('DB에 연결되었습니다.');
+
+    const result = await Question.insertMany(questionsData);
+    console.log(`${result.length}개의 질문이 성공적으로 삽입되었습니다.`);
+  } catch (error) {
+    console.error('데이터 삽입 중 오류 발생:', error);
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+insertMockData();
