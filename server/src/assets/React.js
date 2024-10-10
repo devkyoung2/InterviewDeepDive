b6498c2f1b45b932df9eee7d611e04cab778e66b
devkyoung2 <devkyoung2@gmail.com>
2024-10-10 17:09:18 +0900

    [feat] : 질문 목록 업데이트

---
+++ server/src/assets/React.js
@@ -0,0 +1,92 @@
+const questionsData = [
+  {
+    question: '여러 개발 툴 중 React를 사용하는 이유?',
+    category: 'React',
+  },
+  {
+    question: 'JSX에 대해서 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: 'Props와 State의 차이점에 대해 설명해주세요',
+    category: 'React',
+  },
+  { question: 'PropsDrilling에 대해 설명해주세요', category: 'React' },
+  {
+    question: 'Virtual DOM이 뭔가요?',
+    category: 'React',
+  },
+  {
+    question: '리액트의 라이프사이클에 대해 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: '엘리먼트와 컴포넌트의 차이에 대해 설명해주세요.',
+    category: 'React',
+  },
+  {
+    question:
+      '클래스형 컴포넌트와 함수형 컴포넌트의 차이점에 대해 설명해주세요',
+    category: 'React',
+  },
+  { question: '고차 컴포넌트에 대해 설명해주세요', category: 'React' },
+  {
+    question: '사용해본 상태관리 라이브러리가 어떻게 되나요?',
+    category: 'React',
+  },
+  {
+    question: '클라이언트 상태와 서버 상태의 차이점에 대해 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: '리액트의 에러 바운더리에 대해서 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: 'Suspense에 대해서 알고있나요 ?',
+    category: 'React',
+  },
+  {
+    question: 'React hook에 대해서 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: '커스텀 훅을 사용하는 이유와 장점에 대해서 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: 'useState와 useReducer를 선택하는 기준이 뭔가요?',
+    category: 'React',
+  },
+  {
+    question:
+      'useContext를 사용하여 상태를 공유할 때의 장점과 주의할 점에 대해 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: '복잡한 상황에서 컴포넌트를 설계하는 전략에 대해 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: '코드 스플리팅에 대해서 설명해주세요',
+    category: 'React',
+  },
+  {
+    question: '리액트의 성능 문제를 분석할 때 사용하는 도구와 기법?',
+    category: 'React',
+  },
+  {
+    question: 'Virtual DOM이 성능을 최적화 하는 방법이 뭔가요?',
+    category: 'React',
+  },
+  {
+    question: '리액트에서 사용되는 Key는 무엇인지 설명해주세요',
+    category: 'React',
+  },
+  { question: 'useEffect에 대해서 설명해주세요', category: 'React' },
+  { question: 'useContext에 대해서 설명해주세요', category: 'React' },
+  { question: 'useReducer에 대해서 설명해주세요', category: 'React' },
+  { question: 'useMemo에 대해서 설명해주세요', category: 'React' },
+  { question: 'useCallback에 대해서 설명해주세요', category: 'React' },
+  { question: 'useRef에 대해서 설명해주세요', category: 'React' },
+];
---
+++ server/src/assets/studyfor1010.js
@@ -0,0 +1,59 @@
+const questionsData = [
+  { question: "'this'에 대해 아는대로 설명해주세요", category: 'JavaScript' },
+  {
+    question: '자바스크립트에서 불변성은 무엇을 의미할까요?',
+    category: 'JavaScript',
+  },
+  {
+    question: '자바스크립트 자료형 중에 객체와 다른 타입 간의 차이가 있을까요?',
+    category: 'JavaScript',
+  },
+  { question: '프로토타입이란 무엇인가요?', category: 'JavaScript' },
+  {
+    question: 'Map과 Set에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '얕은 복사와 깊은 복사의 차이점에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: 'JavaScript Module System과 Dynamic import?',
+    category: 'JavaScript',
+  },
+  {
+    question: 'node.js와 그냥 js의 차이점에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  { question: '유사 배열 객체에 대해 설명해주세요', category: 'JavaScript' },
+  { question: 'generator에 대해 설명해주세요', category: 'JavaScript' },
+  { question: 'polyfill이란 무엇인가요?', category: 'JavaScript' },
+  {
+    question: '자바스크립트 엔진의 동작 방법에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '이벤트 루프와 Task queue에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question: '호이스팅(hoisting)에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  { question: '실행 컨텍스트에 대해 설명해주세요', category: 'JavaScript' },
+  { question: '스코프에 대해 아는대로 설명해주세요', category: 'JavaScript' },
+  {
+    question: 'JS의 가비지 컬렉션(GC)에 대해 설명해주세요',
+    category: 'JavaScript',
+  },
+  {
+    question:
+      '자바스크립트는 싱글 스레드 기반인데 어떻게 비동기 처리를 할 수 있나요?',
+    category: 'JavaScript',
+  },
+  {
+    question:
+      '블록 레벨 스코프와 함수 레벨 스코프의 차이점에 대해 설명해주세요.',
+    category: 'JavaScript',
+  },
+];
