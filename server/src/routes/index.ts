6e5ba72ad72c587293c1420ee11528fe740cb36f
devkyoung2 <devkyoung2@gmail.com>
2024-10-30 18:43:55 +0900

    [feat] : 30일 스터디 대비 api 구현

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,11 +3,12 @@ import express from 'express';
 import {
   getRandomQuestion,
   getRandomFor1010,
+  getRandomFor1030,
 } from '../controllers/randomQuestionController';
 
 const router = express.Router();
 
 // router.get('/random-question', getRandomQuestion);
-router.get('/random-question', getRandomFor1010);
+router.get('/random-question', getRandomFor1030);
 
 export default router;
